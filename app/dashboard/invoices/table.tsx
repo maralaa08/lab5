237178c294e62c738fefe11d46a20b67c291b3b0
maralaa08 <Mamaralmaa66@gmail.com>
2024-03-31 23:57:14 +0800

    .

---
+++ app/dashboard/invoices/table.tsx
@@ -0,0 +1,12 @@
+import { fetchFilteredInvoices } from '@/app/lib/data';
+
+export default async function InvoicesTable({
+    query,
+    currentPage,
+  }: {
+    query: string;
+    currentPage: number;
+  }) {
+    const invoices = await fetchFilteredInvoices(query, currentPage);
+    // ...
+  }
